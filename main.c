18e9f3dcc38f34fd668a4328ef8d98f319973e65
Ariel Schwalb <ajschwalb@gmail.com>
2021-12-01 20:27:42 +0200

    my first c project

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char charactorName[] = "Riley";
+    int charactorAge = 18;
+    printf("There once was a woman named %s\n", charactorName);
+    printf("she was %d years old.\n", charactorAge);
+    printf("She really liked the name %s\n", charactorName);
+    printf("but she did not like being %d.\n", charactorAge);
+
+
+    return 0;
+}
